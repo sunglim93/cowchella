6e4134199a79537b3d3fd1a37b4f7ba20931abd8
Travis Peterson <travisp0027@gmail.com>
2023-12-06 19:01:19 -0800

    add landscape & sky

--- mesh.js
+++ mesh.js
@@ -21,6 +21,159 @@ class Mesh {
         this.program = program;
     }
 
+    static from_heightmap(gl, program, map, min, max, material) {
+        let rows = map.length;
+        let cols = map[0].length;
+
+        const MIN_HEIGHT_COLOR = 0.2;
+
+        let off_x = cols /2;
+        let off_z = rows /2;
+        
+        let verts = [];
+        let indis = [];
+
+        function color(height) {
+            let normed_height = (height - min) / (max - min);
+            return MIN_HEIGHT_COLOR + normed_height;
+        }
+        let indi_start = 0;
+        for(let row = 1; row < rows; row++) {
+            for(let col = 1; col < cols; col++) { 
+                let pos_tl = map[row-1][col-1];
+                let pos_tr = map[row-1][col];
+                let pos_bl = map[row][col-1];
+                let pos_br = map[row][col];
+
+                let v_tl = new Vec4(-1, pos_tl, -1, 1);
+                let v_tr = new Vec4(0, pos_tr, -1, 1);
+                let v_bl = new Vec4(-1, pos_bl, 0, 1);
+                let v_br = new Vec4(0, pos_br, 0, 1);
+
+                let normal_t1 = Vec4.normal_of_triangle(v_tl, v_tr, v_bl);
+                let t1_mag = normal_t1.length();
+                normal_t1 = new Vec4((normal_t1.x)/t1_mag, (normal_t1.y)/t1_mag,(normal_t1.z)/t1_mag);
+                let normal_t2 = Vec4.normal_of_triangle(v_br, v_bl, v_tr);
+                let t2_mag = normal_t2.length();
+                normal_t2 = new Vec4((normal_t2.x)/t2_mag, (normal_t2.y)/t2_mag,(normal_t2.z)/t2_mag);
+
+                v_tl.x += col - off_x;
+                v_tl.z += row - off_z;
+
+                v_tr.x += col - off_x;
+                v_tr.z += row - off_z;
+
+                v_bl.x += col - off_x;
+                v_bl.z += row - off_z;
+
+                v_br.x += col - off_x;
+                v_br.z += row - off_z;
+
+                function push_vert(list, vert, u, v, norm){
+                    list.push(vert.x);
+                    list.push(vert.y);
+                    list.push(vert.z);
+                    list.push(1,1,1,1);
+                    list.push(u);
+                    list.push(v);
+                    list.push(norm.x);
+                    list.push(norm.y);
+                    list.push(norm.z);
+                }
+
+                push_vert(verts, v_tl, 0,1, normal_t1);
+                push_vert(verts, v_tr, 1,1, normal_t1);
+                push_vert(verts, v_bl, 0,0, normal_t1);
+
+                push_vert(verts, v_br, 1,0, normal_t2);
+                push_vert(verts, v_bl, 0,0, normal_t2);
+                push_vert(verts, v_tr, 1,1, normal_t2);
+
+                for(let i = 0; i < 6; i++) {
+                    indis.push(indi_start++);
+                }
+            }
+        }
+
+        return new Mesh(gl, program, verts, indis, material, true);
+
+    }
+
+    static inside_box(gl, program, width, height, depth, material){
+        let hwidth = width / 2.0;
+        let hheight = height / 2.0;
+        let hdepth = depth / 2.0;
+
+            //let normals = new Vec4(x, y, z, 0.0).norm();
+            //verts.push(normals.x, normals.y, normals.z);
+
+        let one = new Vec4(hwidth, -hheight, -hdepth, 0.0).norm();
+        let two = new Vec4(-hwidth, -hheight, -hdepth, 0.0).norm();
+        let three = new Vec4(-hwidth, hheight, -hdepth, 0.0).norm();
+        let four = new Vec4(hwidth, hheight, -hdepth, 0.0).norm();
+        let five = new Vec4(hwidth, -hheight, hdepth, 0.0).norm();
+        let six = new Vec4(-hwidth, -hheight, hdepth, 0.0).norm();
+        let seven = new Vec4(-hwidth, hheight, hdepth, 0.0).norm();
+        let eight = new Vec4(hwidth, hheight, hdepth, 0.0).norm();
+
+        let verts = [
+            //x, y, z, r, g, b, a, u, v, normals
+            hwidth, -hheight, -hdepth, 1.0, 0.0, 0.0, 1.0,    0.25  , 0.5  , -one.x, -one.y, -one.z,//0
+            hwidth, -hheight, -hdepth, 1.0, 0.0, 0.0, 1.0,    0.25 , 0.5 , -one.x, -one.y, -one.z,//1
+            hwidth, -hheight, -hdepth, 1.0, 0.0, 0.0, 1.0,    0.5 , 0.75 , -one.x,-one.y,-one.z, //2
+
+            -hwidth, -hheight, -hdepth, 0.0, 1.0, 0.0, 1.0,   0 , 0.5 , -two.x,-two.y,-two.z,//3
+            -hwidth, -hheight, -hdepth, 0.0, 1.0, 0.0, 1.0,   1 , 0.5 , -two.x, -two.y, -two.z,//4
+            -hwidth, -hheight, -hdepth, 0.0, 1.0, 0.0, 1.0,   0.75 , 0.75 , -two.x,-two.y,-two.z,//5
+
+            -hwidth, hheight, -hdepth, 0.0, 0.0, 1.0, 1.0,   0 , 0.25 , -three.x, -three.y, -three.z,//6 
+            -hwidth, hheight, -hdepth, 0.0, 0.0, 1.0, 1.0,   0.75 , 0 ,-three.x, -three.y, -three.z,//7
+            -hwidth, hheight, -hdepth, 0.0, 0.0, 1.0, 1.0,   1 , 0.25 ,-three.x, -three.y, -three.z,//8
+
+            hwidth, hheight, -hdepth, 1.0, 1.0, 0.0, 1.0,    0.25 , 0.25 , -four.x, -four.y, -four.z,//9
+            hwidth, hheight, -hdepth, 1.0, 1.0, 0.0, 1.0,    0.25 , 0.25 ,-four.x, -four.y, -four.z,//10
+            hwidth, hheight, -hdepth, 1.0, 1.0, 0.0, 1.0,    0.5 , 0 , -four.x, -four.y, -four.z,//11  
+
+            hwidth, -hheight, hdepth, 1.0, 0.0, 1.0, 1.0,    0.5 , 0.5 , -five.x, -five.y, -five.z,//12
+            hwidth, -hheight, hdepth, 1.0, 0.0, 1.0, 1.0,    0.5 , 0.5 , -five.x, -five.y, -five.z,//13
+            hwidth, -hheight, hdepth, 1.0, 0.0, 1.0, 1.0,    0.5 , 0.5 ,-five.x, -five.y, -five.z,//14
+
+            -hwidth, -hheight, hdepth, 0.0, 1.0, 1.0, 1.0,   0.75 , 0.5 , -six.x, -six.y, -six.z, //15
+            -hwidth, -hheight, hdepth, 0.0, 1.0, 1.0, 1.0,   0.75 , 0.5 ,-six.x, -six.y, -six.z, //16
+            -hwidth, -hheight, hdepth, 0.0, 1.0, 1.0, 1.0,   0.75 , 0.5 ,-six.x, -six.y, -six.z, //17
+
+            -hwidth, hheight, hdepth, 0.5, 0.5, 1.0, 1.0,    0.75 , 0.25 ,-seven.x, -seven.y, -seven.z,//18
+            -hwidth, hheight, hdepth, 0.5, 0.5, 1.0, 1.0,    0.75 , 0.25 ,-seven.x, -seven.y, -seven.z,//19
+            -hwidth, hheight, hdepth, 0.5, 0.5, 1.0, 1.0,    0.75 , 0.25 ,-seven.x, -seven.y, -seven.z, //20
+
+            hwidth, hheight, hdepth, 1.0, 1.0, 0.5, 1.0,     0.5 , 0.25 ,-eight.x, -eight.y, -eight.z,//21
+            hwidth, hheight, hdepth, 1.0, 1.0, 0.5, 1.0,     0.5 , 0.25 , -eight.x, -eight.y, -eight.z,//22
+            hwidth, hheight, hdepth, 1.0, 1.0, 0.5, 1.0,     0.5 , 0.25 , -eight.x, -eight.y, -eight.z,//23
+        ];
+
+        let indis = [
+            // clockwise winding
+            /*
+            0, 1, 2, 2, 3, 0, 
+            4, 0, 3, 3, 7, 4, 
+            5, 4, 7, 7, 6, 5, 
+            1, 5, 6, 6, 2, 1,
+            3, 2, 6, 6, 7, 3,
+            4, 5, 1, 1, 0, 4,
+            */
+
+            // counter-clockwise winding
+            0, 9, 3, 3, 9, 6, //front
+            12, 21, 1, 1, 21, 10, //right
+            15, 18, 13, 13, 18, 22, //back
+            4, 8, 16, 16, 8, 20, //left
+            19, 7, 23, 23, 7, 11, //top
+            5, 17, 2, 2, 17, 14 //bottom
+        ];
+        gl.bindTexture(gl.TEXTURE_2D, material.texture);
+
+        return new Mesh( gl, program, verts, indis, material);
+    }
     /**
      * Create a box mesh with the given dimensions and colors.
      * @param {WebGLRenderingContext} gl 
@@ -105,9 +258,11 @@ class Mesh {
         return new Mesh( gl, program, verts, indis, material);
     }
 
-    static make_uv_sphere(gl, program, subdivs, material){
+    static make_uv_sphere(gl, program, subdivs, material, invert_normals){
         let verts = [];
         let indis = [];
+        let n = 1;
+        if(invert_normals) n = -1;
         const TAU = Math.PI * 2;
         //for each layer
         for (let layer = 0; layer <=subdivs; layer++){
@@ -127,7 +282,7 @@ class Mesh {
                 verts.push(1.0,1.0,1.0,1.0);
                 verts.push(u,v);
                 let normals = new Vec4(x, y, z, 0.0).norm();
-                verts.push(normals.x, normals.y, normals.z);
+                verts.push( n * normals.x, n * normals.y, n * normals.z);
             }
 
             if (layer > 0){
